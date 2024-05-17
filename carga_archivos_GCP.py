d2ecc00aa5ab116c22b92da12697f008774fcf87
unknown <nelvinvelazco@gmail.com>
2024-05-16 23:38:48 -0500

    funcion2 ELT Yelp

--- carga_archivos_GCP.py
+++ carga_archivos_GCP.py
@@ -13,7 +13,7 @@ def subir_to_gcp(bucket_name, source_file_name, destination_blob_name):
     print(f'File {source_file_name} uploaded to {destination_blob_name} in {bucket_name}.')
 
 
-ruta_archivos = "C:/Data/GMaps/"     # Ruta de los json de Sitios
+ruta_archivos = "D:/PROYECTO FINAL/Google Maps/metadata-sitios/"     # Ruta de los json de Sitios
 archivos_json = [[os.path.join(ruta_archivos, archivo), archivo] for archivo in os.listdir(ruta_archivos) if archivo.endswith('.json')]
 bucket_name = 'gmaps_data2'
 
@@ -21,6 +21,7 @@ lista_df_sitios = []
 for archivo in archivos_json:           
     source_file_name = archivo[0]
     destination_blob_name = archivo[1]
-    # Upload the CSV file to GCS
+    
+    # Llama a la funcion que sube el archivo
     subir_to_gcp(bucket_name, source_file_name, destination_blob_name)
     print(f"SE SUBIO EL ARCHIVO: {destination_blob_name}")
